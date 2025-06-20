d2f7dd7aa7d064a01c9655f0bf5568d13184102f
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-06-21 01:42:53 +0530

    implemented create end point for user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,7 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { User } from "../entities/user.entity";
 
- interface IUser {
-  
+interface IUser {
   first_name: string;
   last_name: string;
   email: string;
@@ -11,8 +10,50 @@ import { User } from "../entities/user.entity";
   profile_img?: string | null; // nullable
 }
 
+export const createUser = async (
+  req: Request<{}, {}, IUser>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { first_name, last_name, email, mobile_no, role, profile_img } =
+      req.body;
 
-export const createUser=async(req:Request<{},{},IUser>,res:Response,next:NextFunction)=>{
-    const {first_name} = req.body
-    res.json(`got this body :${first_name}` )
-}
+    //check for existane
+    const userExists = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    console.log(userExists)
+
+    if (userExists) {
+      res.status(409).json({
+        message: `A user with this email already exists.${userExists.email}`,
+      });
+
+      return;
+    }
+
+    const user = User.create({
+      first_name,
+      last_name,
+      email,
+      mobile_no,
+      role,
+      profile_img,
+    });
+
+    const createdUser = await user.save();
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: createdUser.user_id,
+        name: `${createdUser.first_name} ${createdUser.last_name}`,
+        email: createdUser.email,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
