138de6ab5b829e1c9cb31899b6beddd71dbbf11a
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-06-22 02:22:03 +0530

    some unsaved changes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { UserService } from "../services/user.service";
 import { IUser } from "../interfaces/IUser.interface";
 
+
 const userService = new UserService();
 
 // Create user
