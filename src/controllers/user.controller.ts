bfe547447e3708b33d5cb27233d73a84e6e2330d
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-06-21 11:15:19 +0530

    implemented getUsers end point with pagination

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -10,6 +10,7 @@ interface IUser {
   profile_img?: string | null; // nullable
 }
 
+//create user function being exported
 export const createUser = async (
   req: Request<{}, {}, IUser>,
   res: Response,
@@ -25,7 +26,7 @@ export const createUser = async (
         email: email,
       },
     });
-    console.log(userExists)
+    
 
     if (userExists) {
       res.status(409).json({
@@ -51,6 +52,39 @@ export const createUser = async (
         id: createdUser.user_id,
         name: `${createdUser.first_name} ${createdUser.last_name}`,
         email: createdUser.email,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+//read user function being exported
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+    const skip = (page - 1) * limit;
+
+    const [users, count] = await User.findAndCount({
+      skip,
+      take: limit,
+      order: {
+        first_name: "ASC",
+      },
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Users fetched successfully",
+      data: {
+        users,
+        meta: {
+          total: count,
+          page,
+          last_page: Math.ceil(count / limit),
+        },
       },
     });
   } catch (err) {
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+
 import { createUser } from "../controllers/user.controller";
+import { getUsers } from "../controllers/user.controller";
+
 import { CreateUserDto } from "../DTOs/create-user.dto";
 import { validateDto } from "../middlewares/validateDto";
 
@@ -7,6 +10,7 @@ const userRouter = Router();
 
 //define user routes
 userRouter.post('/',validateDto(CreateUserDto),createUser);
+userRouter.get('/',getUsers);
 
 export default userRouter;
 
